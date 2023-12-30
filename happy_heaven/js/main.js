78a8b02425acefb489ffc8b7291399dfa98da7aa
CHIEH-JU <linjru1010@gmail.com>
2023-12-31 01:32:52 +0800

    1231

--- happy_heaven/js/main.js
+++ happy_heaven/js/main.js
@@ -6,3 +6,19 @@ $("#rabbit_hover").click(function () {
   $(".active2").css("visibility", "visible");
   $(".hover2").css("visibility", "hidden");
 });
+$("#seal_hover").click(function () {
+  $(".active3").css("visibility", "visible");
+  $(".hover3").css("visibility", "hidden");
+});
+$("#turtle_hover").click(function () {
+  $(".active4").css("visibility", "visible");
+  $(".hover4").css("visibility", "hidden");
+});
+$("#shark_hover").click(function () {
+  $(".active5").css("visibility", "visible");
+  $(".hover5").css("visibility", "hidden");
+});
+$("#goose_hover").click(function () {
+  $(".active6").css("visibility", "visible");
+  $(".hover6").css("visibility", "hidden");
+});
