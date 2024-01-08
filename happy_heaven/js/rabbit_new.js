8b545354565e134b85890be3c27a3ac4ecaf5b72
CHIEH-JU <linjru1010@gmail.com>
2024-01-08 13:18:40 +0800

    0108

---
+++ happy_heaven/js/rabbit_new.js
@@ -0,0 +1,8 @@
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  sectionsColor: ["#000", "#F1F1F1", "#000", "#F1F1F1"],
+  navigation: true,
+  slidesNavigation: true,
+});
